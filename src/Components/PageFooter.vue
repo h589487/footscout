5f08fae6902d6eb92a6c45f030a257d6cf407d26
John Chen <john.chen@omega365.com>
2024-07-12 09:32:01 +0200

    PageFooter og PageHeader lagt til

--- src/Components/PageFooter.vue
+++ src/Components/PageFooter.vue
@@ -1,9 +1,35 @@
-<script setup>
-</script>
-
 <template>
-
+  <footer>
+    <div class="footer-content">
+      <p>FootScout A/S</p>
+      <p>PhoneNumber: 123-45-511-13</p>
+      <p>Email: footscout@customerservice.com</p>
+      <p>Address: FootScout building 1, 6127 FootStreet</p>
+    </div>
+  </footer>
 </template>
 
+<script setup>
+// No script content needed for now
+</script>
+
 <style scoped>
+footer {
+  background-color: #001f3f; /* Marineblå farge */
+  color: #fff;
+  text-align: center;
+  padding: 0.025rem 0.0125rem;
+  position: fixed;
+  width: 100%;
+  bottom: 0;
+}
+.footer-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 0.4rem; /* Mindre skriftstørrelse */
+}
+.footer-content p {
+  margin: 0.5rem 0;
+}
 </style>
